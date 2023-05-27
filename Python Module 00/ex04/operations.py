8f3cfa4b29b82e00ebddf3f2fbf912666a2b08de
Lucía López Fuentes <108120620+luciallf@users.noreply.github.com>
2023-05-27 10:16:24 +0200

    Update operations.py

--- Python Module 00/ex04/operations.py
+++ Python Module 00/ex04/operations.py
@@ -12,31 +12,36 @@
 
 
 import sys
+
 def operations(num1, num2):
-    sum, dif, prod=num1+num2, num1-num2, num1*num2 
-    print("Sum:", sum)
-    print("Difference:", dif)
-    print("Product:", prod)
-    if num2==0:
-        print("Quotient: Error, division by zero")
-        print ("Remainder: Error modulo by zero")  
+    """
+    Esta función realiza operaciones aritméticas básicas entre dos números.
+    """
+    sum, dif, prod = num1 + num2, num1 - num2, num1 * num2
+    print("Suma:", sum)
+    print("Diferencia:", dif)
+    print("Producto:", prod)
+
+    if num2 == 0:
+        # Comprueba si el segundo número es cero para evitar una división por cero
+        print("Cociente: Error, división por cero")
+        print("Resto: Error, módulo por cero")
         print("")
     else:
-        quotient = num1/num2
-        Remainder =num1%num2 
-        print("Quotient:", quotient)
-        print("Remainder", Remainder)  
+        quotient = num1 / num2
+        remainder = num1 % num2
+        print("Cociente:", quotient)
+        print("Resto:", remainder)
         print("")
 
 
 if __name__ == "__main__":
-    if len(sys.argv)<=1:
+    if len(sys.argv) <= 1:
         print()
-    elif len(sys.argv)!=3:
-        print("Error: please write 2 numbers arguments \n")
+    elif len(sys.argv) != 3:
+        print("Error: por favor, proporciona 2 argumentos numéricos\n")
     else:
         try:
             operations(int(sys.argv[1]), int(sys.argv[2]))
         except:
-            print("AsertionError: only integers \n")
-  
+            print("Error de Asertión: solo se admiten números enteros\n")
