9aaf417ae9e27aa6e15210b6d5f56091de638201
Lucía López Fuentes <108120620+luciallf@users.noreply.github.com>
2023-05-27 10:19:12 +0200

    Update kata04.py

--- Python Module 00/ex05/kata04.py
+++ Python Module 00/ex05/kata04.py
@@ -13,5 +13,12 @@
 
 
 kata = (0, 4, 123.42222, 10000, 1235.67)
+
+# Imprime una cadena formateada con los elementos de la tupla kata
+# Utiliza "{:02d}" para imprimir los números enteros con dos dígitos, rellenando con ceros si es necesario
+# Utiliza "{:.02f}" para imprimir el número de punto flotante con dos decimales
+# Utiliza "{:.02e}" para imprimir el número en notación científica con dos decimales
+
 print("module_{:02d}, ex_{:02d} : {:.02f}, {:.02e}, {:.02e}".format(kata[0], kata[1], kata[2], kata[3], kata[4]))
+
 
