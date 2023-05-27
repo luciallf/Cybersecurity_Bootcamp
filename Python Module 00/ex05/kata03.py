646592eac5df373f8515c988ac2b5ad8fa1cef54
Lucía López Fuentes <108120620+luciallf@users.noreply.github.com>
2023-05-27 10:18:39 +0200

    Update kata03.py

--- Python Module 00/ex05/kata03.py
+++ Python Module 00/ex05/kata03.py
@@ -11,4 +11,9 @@
 # **************************************************************************** #
 
 kata = "The right format"
+
+# Imprime una cadena formateada con el contenido de la variable kata
+# Utiliza "{:->42}" para justificar el texto a la derecha y rellenar con guiones (-) hasta alcanzar una longitud de 42 caracteres
+# El parámetro end="" se utiliza para evitar el salto de línea al final de la impresión
+
 print("{:->42}".format(kata), end="")
