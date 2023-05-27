10e322315f6e63b34675890c1341e0ed6fb6e27a
Lucía López Fuentes <108120620+luciallf@users.noreply.github.com>
2023-05-27 10:17:54 +0200

    Update kata02.py

--- Python Module 00/ex05/kata02.py
+++ Python Module 00/ex05/kata02.py
@@ -12,4 +12,9 @@
 
 
 kata = (2019, 9, 25, 3, 30)
+
+# Imprime una cadena formateada con los elementos de la tupla kata
+# Se utiliza "{:02d}" para imprimir los números con dos dígitos, rellenando con ceros si es necesario
+# "{}" se utiliza para imprimir los elementos que no necesitan formato especial
+
 print("{:02d}/{}/{} {:02d}:{}".format(kata[1], kata[2], kata[0], kata[3], kata[4]))
