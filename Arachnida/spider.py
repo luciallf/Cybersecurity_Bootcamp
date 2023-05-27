7ab40cb1dc87f6348ce2b4f68cba374236619fce
Lucía López Fuentes <108120620+luciallf@users.noreply.github.com>
2023-05-27 10:31:19 +0200

    Update spider.py

--- Arachnida/spider.py
+++ Arachnida/spider.py
@@ -11,29 +11,32 @@
 # **************************************************************************** #
 
 
-import argparse 
-import requests 
-import os 
-from bs4 import BeautifulSoup as sopa 
-from urllib.parse import urlparse, urljoin 
+import argparse
+import requests
+import os
+from bs4 import BeautifulSoup as sopa
+from urllib.parse import urlparse, urljoin
 
-urls_webs = set()
-urls_imag = set()
-
+urls_webs = set()  # Conjunto para almacenar las URLs de las páginas web
+urls_imag = set()  # Conjunto para almacenar las URLs de las imágenes
 
 def extraer_url(sitioweb, niv):
+    """
+    Esta función extrae las URLs de las páginas web dentro del sitio web dado.
+    Recursivamente se llama a sí misma para seguir extrayendo URLs hasta alcanzar el nivel máximo de profundidad.
+    """
     try:
-        r = requests.get(sitioweb)   
+        r = requests.get(sitioweb)
     except:
         print("Se ha producido un error: no se puede abrir el URL", sitioweb)
     else:
         if r.status_code == 200:
-            xml = sopa(r.content, "html.parser") 
+            xml = sopa(r.content, "html.parser")
             enlaces = [enlace.get("href") for enlace in xml.find_all("a") if enlace.has_attr("href") and (enlace["href"].startswith("http://") or enlace["href"].startswith("https://"))]
             urlparseada = urlparse(sitioweb)
             esquema = urlparseada.scheme
             dominio = urlparseada.netloc
-            ruta = urlparseada.path 
+            ruta = urlparseada.path
             if int(niv) < 1:
                 return
             else:
@@ -55,9 +58,10 @@ def extraer_url(sitioweb, niv):
                                 extraer_url(enlace_completo, niv - 1)
             return esquema, dominio, ruta
 
-
-
 def descargar_imagen(contains):
+    """
+    Esta función descarga las imágenes encontradas en las URLs de las páginas web.
+    """
     for elements in urls_webs:
         try:
             i = requests.get(elements)
@@ -67,73 +71,66 @@ def descargar_imagen(contains):
         except:
             continue
         else:
-            urllink = [img['src'] for img in img_tags] 
+            urllink = [img['src'] for img in img_tags]
             for url in urllink:
                 if url in urls_imag:
                     continue
                 else:
                     try:
-                        url_imag= requests.get(url)
+                        url_imag = requests.get(url)
                     except:
-                        continue   
+                        continue
                     else:
+                        extensiones = ['.jpg', '.jpeg', '.png', '.gif', '.bmp']
                         if not any(ext in url for ext in extensiones):
                             continue
                         else:
-                            nombre = url.split("/")[-1] 
+                            nombre = url.split("/")[-1]
                             if not os.path.exists(folder):
                                 os.makedirs(folder)
-                            with open(folder + "/" + nombre, "wb") as archivo: 
+                            with open(folder + "/" + nombre, "wb") as archivo:
                                 archivo.write(url_imag.content)
                         urls_imag.add(url)
 
-          
-
-if __name__ == "__main__": 
-
-    extensiones = ['.jpg', '.jpeg', '.png', '.gif', '.bmp'] 
-    
+if __name__ == "__main__":
+    extensiones = ['.jpg', '.jpeg', '.png', '.gif', '.bmp']
 
     analizador = argparse.ArgumentParser(
-    description = "Herramienta de scraping de imagenes en un sitio web"
+        description="Herramienta de scraping de imágenes en un sitio web"
     )
-    analizador.add_argument("URL", help="URL del sitio web donde se quieren descargar las imagenes") 
-    analizador.add_argument("-r", help= "descarga de forma recursiva de las imágenes en la URL recibida como parámetro ", action= 'store_true') 
-    analizador.add_argument("-l", help="indica el nivel de prrofundidad maximo de la descarga recursiva", type= int, default= 5 ) 
-    analizador.add_argument("-p", help= "indica la ruta donde se guardarán los archivos descargados.", type= str, default= "./data/") 
-    analizador.add_argument("-o", help = "Muestra las URLS al terminar", action="store_true" )
+    analizador.add_argument("URL", help="URL del sitio web donde se quieren descargar las imágenes")
+    analizador.add_argument("-r", help="descarga de forma recursiva de las imágenes en la URL recibida como parámetro", action='store_true')
+    analizador.add_argument("-l", help="indica el nivel de profundidad máximo de la descarga recursiva", type=int, default=5)
+    analizador.add_argument("-p", help="indica la ruta donde se guardarán los archivos descargados.", type=str, default="./data/")
+    analizador.add_argument("-o", help="Muestra las URLS al terminar", action="store_true")
 
-    
     args = analizador.parse_args()
 
-    global urls, folder, niv, recursiv, orden 
-    
-    
+    global urls, folder, niv, recursiv, orden
+
     urls = args.URL
     folder = args.p
-    niv = int(args.l) 
+    niv = int(args.l)
     recursiv = args.r
     orden = args.o
 
     if str.startswith(folder, "http") or str.startswith(folder, "file://"):
-        urls = folder 
+        urls = folder
         folder = "./data/"
-    
-    
-    try: 
+
+    try:
         print("Analizando las páginas..")
         if recursiv:
             extraer_url(urls, niv)
         else:
             urls_webs.add(urls)
         print(f'Descargando imágenes de {urls}...')
-        
+
         descargar_imagen(urls_imag)
-        
+
         for link in urls_webs.copy():
             filename = os.path.basename(link)
             file_path = os.path.join(folder, filename)
             extraer_url(link, niv)
     except:
-        print("Error, no se puede leer la URL correctamente")   
-        
+        print("Error, no se puede leer la URL correctamente")
