83450800300ba7bfb34a471523b54237393aabc3
Lucía López Fuentes <108120620+luciallf@users.noreply.github.com>
2023-05-27 10:09:40 +0200

    Update exec.py

--- Python Module 00/ex01/exec.py
+++ Python Module 00/ex01/exec.py
@@ -1,15 +1,20 @@
-#!/usr/bin/env python
-
-import sys 
+import sys
 
 def reverse_words(string):
+    # Invertir la cadena
     reversed_string = string[::-1]
+    # Cambiar el caso de las letras en la cadena invertida
     swapped_case_string = reversed_string.swapcase()
     return swapped_case_string
 
 if len(sys.argv) > 1:
+    # Obtener la cadena de texto de los argumentos de línea de comandos
     string = ' '.join(sys.argv[1:])
+    # Llamar a la función reverse_words() con la cadena de texto
     result = reverse_words(string)
+    # Imprimir el resultado
     print(result)
 else:
+    # Imprimir mensaje de uso en caso de que no se proporcionen argumentos
     print("usage: ./exec.py num num")
+
