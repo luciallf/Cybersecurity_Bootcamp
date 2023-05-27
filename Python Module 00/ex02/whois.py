9c7ed1a9bf9a9c75ab3b2fad7370517ba14c11d1
Lucía López Fuentes <108120620+luciallf@users.noreply.github.com>
2023-05-27 10:14:04 +0200

    Update whois.py

--- Python Module 00/ex02/whois.py
+++ Python Module 00/ex02/whois.py
@@ -15,15 +15,22 @@
 import sys
 
 if len(sys.argv) == 2:
+    # Comprueba si se proporcionó exactamente un argumento en línea de comandos
     try:
+        # Intenta convertir el argumento a un número entero
         num = int(sys.argv[1])
         if num == 0:
+            # Si el número es 0, imprime "I'm Zero."
             print("I'm Zero.")
         elif num % 2 == 0:
+            # Si el número es par, imprime "I'm Even."
             print("I'm Even.")
         else:
+            # Si el número es impar, imprime "I'm Odd."
             print("I'm Odd.")
     except ValueError:
+        # Si el argumento no se puede convertir a entero, imprime un mensaje de error
         print("Error: Argument is not an integer.")
 else:
+    # Si no se proporciona exactamente un argumento, imprime un mensaje de error
     print("Error: Provide one integer argument.")
